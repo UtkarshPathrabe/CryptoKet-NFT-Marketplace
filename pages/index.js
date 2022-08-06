7b37a9e9260d4dda0e2fee7c91c0161063d9e3f3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-06 19:35:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { Banner } from '../components';
+
 const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
+  <div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner
+        name="Discover, collect, and sell extraordinary NFTs"
+        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+      />
+    </div>
   </div>
 );
 
