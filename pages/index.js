96a79582a37747bcc1ab5ec41822f98c1591ab1b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-07 11:12:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,14 +60,19 @@ const Home = () => {
               {!hideButtons && (
               <>
                 <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
-                  <Image src={images.left} layout="fill" objectFit="contain" alt="left-arrow" className={(theme === 'light') && 'filter invert'} />
+                  <Image src={images.left} layout="fill" objectFit="contain" alt="left-arrow" className={(theme === 'light') ? 'filter invert' : ''} />
                 </div>
                 <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
-                  <Image src={images.right} layout="fill" objectFit="contain" alt="right-arrow" className={(theme === 'light') && 'filter invert'} />
+                  <Image src={images.right} layout="fill" objectFit="contain" alt="right-arrow" className={(theme === 'light') ? 'filter invert' : ''} />
                 </div>
               </>
               )}
             </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="flexBetween mx-4 ms:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
           </div>
         </div>
       </div>
