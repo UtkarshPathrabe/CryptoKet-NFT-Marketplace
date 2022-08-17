c874947c7729117a5ed5bec6a984b923b2f2eec3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-17 20:45:15 +0530

    Update NFTContext.js

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -2,15 +2,97 @@ import React, { useState, useEffect } from 'react';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
 import axios from 'axios';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
 
 import { MarketAddress, MarketAddressABI } from './constants';
+
+const projectId = process.env.NEXT_PUBLIC_IPFS_PROJECT_ID;
+const projectSecret = process.env.NEXT_PUBLIC_IPFS_API_KEY_SECRET;
+const ipfsHost = process.env.NEXT_PUBLIC_IPFS_API_HOST;
+const basicAuth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
+const subdomain = process.env.NEXT_PUBLIC_IPFS_SUBDOMAIN_NAME;
+
+const client = ipfsHttpClient({
+  host: ipfsHost,
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    authorization: basicAuth,
+  },
+});
 
 export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState('');
   const nftCurrency = 'ETH';
+
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) {
+      alert('Please install MetaMask extension...');
+      return;
+    }
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log('No accounts found');
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert('Please install MetaMask extension...');
+      return;
+    }
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setCurrentAccount(accounts[0]);
+    window.location.reload();
+  };
+
+  const uploadToIPFS = async (file) => {
+    try {
+      const added = await client.add({ content: file });
+      const url = `${subdomain}/ipfs/${added.path}`;
+      console.log('Image file upload URL - ', url);
+      return url;
+    } catch (error) {
+      console.log('Error uploading file to IPFS.');
+    }
+  };
+
+  const createNFT = async (formInput, fileUrl, router) => {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) {
+      return;
+    }
+    const data = JSON.stringify({ name, description, image: fileUrl });
+    try {
+      const added = await client.add(data);
+      const url = `${subdomain}/ipfs/${added.path}`;
+      await createSale(url, price);
+      router.push('/');
+    } catch (error) {
+      console.log('Error uploading file to IPFS.');
+    }
+  };
+
+  const createSale = async (url, formInputPrice, isReselling, id) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice, 'ether');
+    const contract = fetchContract(signer);
+  };
+
   return (
-    <NFTContext.Provider value={{ nftCurrency }}>
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}>
       { children }
     </NFTContext.Provider>
   );
