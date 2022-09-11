1b88c2ad2fa207719713501c1fa796ffa2a21da5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-11 23:44:09 +0530

    Create getTopCreators.js

---
+++ utils/getTopCreators.js
@@ -0,0 +1,12 @@
+export const getCreators = (nfts) => {
+  const creators = nfts.reduce((creatorObject, nft) => {
+    // eslint-disable-next-line no-param-reassign
+    (creatorObject[nft.seller] = creatorObject[nft.seller] || []).push(nft);
+    return creatorObject;
+  }, {});
+  return Object.entries(creators).map((creator) => {
+    const seller = creator[0];
+    const sum = creator[1].map((item) => Number(item.price)).reduce((prev, value) => prev + value, 0);
+    return ({ seller, sum });
+  });
+};
