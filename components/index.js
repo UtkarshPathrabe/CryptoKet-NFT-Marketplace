bac1629fd052eb912969b5e7a5a045a30c48aeb2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-11 14:04:55 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -3,8 +3,9 @@ import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
+import Modal from './Modal';
 import NFTCard from './NFTCard';
 import Input from './Input';
 import Loader from './Loader';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Input, Loader };
+export { Navbar, Footer, Button, Banner, CreatorCard, Modal, NFTCard, Input, Loader };
