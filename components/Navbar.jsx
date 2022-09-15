01514d3a3d9815d7a8854c7308c33bf387a22b46
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-15 18:29:57 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
@@ -60,11 +60,46 @@ const ButtonGroup = ({ router, setActive }) => {
   );
 };
 
+const checkActive = (active, setActive, router) => {
+  switch (router.pathname) {
+    case '/': {
+      if (active !== 'Explore NFTs') {
+        setActive('Explore NFTs');
+      }
+      break;
+    }
+    case '/listed-nfts': {
+      if (active !== 'Listed NFTs') {
+        setActive('Listed NFTs');
+      }
+      break;
+    }
+    case '/my-nfts': {
+      if (active !== 'My NFTs') {
+        setActive('My NFTs');
+      }
+      break;
+    }
+    case '/create-nft': {
+      setActive('');
+      break;
+    }
+    default: {
+      setActive('');
+      break;
+    }
+  }
+};
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const [active, setActive] = useState('Explore NFTs');
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    checkActive(active, setActive, router);
+  }, [router.pathname]);
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
